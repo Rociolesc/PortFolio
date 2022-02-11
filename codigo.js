4e19f41f858fcc86cb5c352f4228d92610432db6
Rociolesc <rociolesc99@gmail.com>
2022-02-11 19:30:32 -0300

    Archivo JS

--- codigo.js
+++ codigo.js
@@ -1,18 +1,19 @@
 let posicion= window.pageYOffset;
-let banner=document.getElementById('n')
+let banner=document.getElementById('cuerpo')
 
 window.addEventListener('scroll', function(){
 	let nuevaPosicion = window.pageYOffset;
 	if (posicion >= nuevaPosicion) {
 		banner.style.top='0px';
-		banner.style.transition='2s';
+		banner.style.transition='1.5s';
 		}
 	else{
-				banner.style.top='-600px';
-				banner.style.transition='2s';
+			
+			banner.style.top='-450px';
+			banner.style.transition='1.5s';
 		}
 	});
 
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
+  window.scrollTo(169, 409);
 }
