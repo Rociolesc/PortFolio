15cb6cbd391638a7f9ad9392c95d81a253941e27
Rociolesc <rociolesc99@gmail.com>
2022-03-10 20:16:34 -0300

    agregamos edits de color y font-size

--- contentEditable.js
+++ contentEditable.js
@@ -1,13 +1,25 @@
 const editBann= document.getElementById('contEditBann');
 const botXeY= document.getElementById('editBanner2');
-const checkBanner= document.getElementById('checkBanner');
 const  infoOriginal=document.getElementById('contEditBann').innerHTML;
 let info=document.getElementById('contEditBann').innerHTML;
+
+const colorYFont=document.getElementById('banner__circulo');
+let colorOrg= colorYFont.style.color='rgba(16, 255, 226, 1)';
+const inpColor=document.getElementById('editBanColor');
+const editColFont=document.getElementById('editColFont');
+let colorImp=inpColor.value;
+
+let fontImp= inpFont.value ;
+let fontOrg=  editBann.style.fontSize='70px';
+const inpFont=document.getElementById('editBanFont');
+
 function editar(){
     editBann.contentEditable='true';
     editBann.disignMode='off';
     botXeY.style.display='inline-block';
     console.log(info);
+    editColFont.style.display='inline-block';
+   
 }
 function check(){
 
@@ -16,14 +28,26 @@ function check(){
     botXeY.style.display='none';
     info=editBann.innerHTML;
     console.log('funk');
-    
+
+    editColFont.style.display='none';
+    colorImp=inpColor.value;
+    colorYFont.style.color=colorImp;
+
+    editBann.style.fontSize=inpFont.value + "px";
+    fontImp= inpFont.value;
 }
 function xEdit(){
     editBann.contentEditable='false'
     editBann.innerHTML=info
     botXeY.style.display='none';
+    editColFont.style.display='none';
+    colorYFont.style.color=colorImp;
 }
 function recOrig(){
     editBann.innerHTML=infoOriginal;
+    colorYFont.style.color=colorOrg;
+    editBann.style.fontSize=fontOrg;
 }
+
+
 
