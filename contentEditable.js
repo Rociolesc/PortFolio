fec7238663551fabb6d801f8f625911f08b2fdf4
Rociolesc <rociolesc99@gmail.com>
2022-03-16 10:13:22 -0300

    funcionalidad del edit 75%

--- contentEditable.js
+++ contentEditable.js
@@ -12,52 +12,212 @@ let colorImp=inpColor.value;
 const inpFont=document.getElementById('editBanFont');
 let fontImp= inpFont.value;
 let fontOrg=  editBann.style.fontSize='70px';*/
-const editables= document.querySelectorAll('#contEditBann, #contEditPerfil,#contEditFormUtn,#contEditFormNac,#contEditFormArP')
-const botonesXeY=document.querySelectorAll('#editBanner2, #editPerfil2,#editFormUtn,#editFormNac,#editFormArP')
-const fontYColor=document.querySelectorAll('#editColFontBanner,#editColFontPerfil,#editColFontFUtn,#editColFontFNac,#editColFontFArP')
-const contColor1=document.querySelectorAll('#banner__circulo')
-const contFont1=document.querySelectorAll('#colorFontPerfil1,#colorFontPerfil2,#colorFontPerfil3')
-const inpColor1=document.querySelectorAll('editBanColor')
-let info=null;
-let colorInp1=inpColor1.value;
 
-const inpColor=document.getElementById('editBanColor');
-function editar(n){
-    editables[n].contentEditable='true';
+
+const editables=document.querySelectorAll('#contEditBann,#contEditPerfil,#contEditFormUtn,#contEditFormNac,#contEditFormArP');
+const botonesXeY=document.querySelectorAll('#editBanner2,#editPerfil2,#editFormUtn2,#editFormNac2,#editFormArP2')
+const fontYColor=document.querySelectorAll('#editColFontBanner,#editColFontPerfil,#editColFontFUtn,#editColFontFNac,#editColFontFArP')
+
+const contColor1=document.querySelectorAll('#banner__circulo,#colorFontPerfil1,#colorFontFUtn1,#colorFontFNac1,#colorFontFArP1')
+const contColor2=document.querySelectorAll('#colorFontPerfil2,#colorFontFUtn2,#colorFontFNac2,#colorFontFArP2')
+const contColor3=document.querySelectorAll('#colorFontPerfil3')
+
+const contFont1=document.querySelectorAll('#contEditBann,#colorFontPerfil1,#colorFontFUtn1,#colorFontFNac1,#colorFontFArP1',)
+const contFont2=document.querySelectorAll('#colorFontPerfil2,#colorFontFUtn2,#colorFontFNac2,#colorFontFArP2')
+const contFont3=document.querySelectorAll('#colorFontPerfil3')
+
+const inpColor1=document.querySelectorAll('#editBanColor,#editPerColor,#editFUtnColor,#editFNacColor,#editFArPColor')
+const inpColor2=document.querySelectorAll('#editPerColor2,#editFUtnColor2,#editFNacColor2,#editFArPColor2')
+const inpColor3=document.querySelectorAll('#editPerColor3')
+
+const inpFont1=document.querySelectorAll('#editBanFont,#editPerFont,#editFUtnFont,#editFNacFont,#editFArPFont')
+const inpFont2=document.querySelectorAll('#editPerFont2,#editFUtnFont2,#editFNacFont2,#editFArPFont2')
+const inpFont3=document.querySelectorAll('#editPerFont3')
+
+
+let info1=null;
+let info2=null;
+let info3=null;
+let colorInp1=null;
+let colorInp2=null;
+let colorInp3=null;
+let fontInp1=null;
+let fontInp2=null;
+let fontInp3=null;
+/*
+let colorInp1=inpColor1.value;
+let verdura =['manzana,pera','limon']
+const inpColor=document.getElementById('editBanColor');*/
+
+
+
+
+    
+function editar(n,l,p){
+    editables[n].contentEditable="true";
     editables[n].disignMode='on';
-    botonesXeY[n].style.display='inline-block';
+    botonesXeY[n].style.display="inline-block";
     fontYColor[n].style.display='inline-block';
     editables[n].style.outline="#5C9F9E 1px solid"
-   
+    info1=contFont1[n].innerHTML;
+    info2=contFont2[l].innerHTML;
+    info3=contFont3[p].innerHTML;
+    
 }
-function check(n){
-
+function check(n,l,p){
     editables[n].contentEditable='false';
     editables[n].disignMode='off';
     botonesXeY[n].style.display='none';
-    info=editables[n].innerHTML;
     editables[n].style.outline="none"
     fontYColor[n].style.display='none';
-    colorInp1=inpColor1.value;
+   /* info1=contFont1[n].innerHTML;
+    info2=contFont2[l].innerHTML;
+    info3=contFont3[p].innerHTML;*/
+
+    colorInp1=inpColor1[n].value;
     contColor1[n].style.color=colorInp1;
+    contFont1[n].style.fontSize=inpFont1[n].value + "px";
     
-   /* editables[n].style.fontSize=inpFont.value + "px";
-    fontImp= inpFont.value;*/
+    contColor2[l].style.color=inpColor2[l].value ;
+    contFont2[l].style.fontSize=inpFont2[l].value + "px";
+  
+    contColor3[p].style.color=inpColor3[p].value;
+    contFont3[p].style.fontSize=inpFont3[p].value + "px";
+    
+
 }
-/*
-function xEdit(n){
-    editables[n].contentEditable='false'
-    editables[n].innerHTML=info
+
+
+function xEdit(n,l,p){
+  
+   
+    editables[n].contentEditable='false';
+    editables[n].disignMode='off';
+   
     botonesXeY[n].style.display='none';
     fontYColor[n].style.display='none';
-    colorYFont.style.color=colorImp;
+    editables[n].style.outline="none"; 
+    contFont1[n].innerHTML=info1;
+    contFont2[l].innerHTML=info2;
+    contFont3[p].innerHTML=info3;
+
+   /* contColor1[n].style.color=inpColor1[n].value;
+    contColor2[l].style.color=inpColor2[l].value ;
+    contColor3[p].style.color=colorInp3;
+
+    contFont1[n].style.fontSize=inpFont1[n].value;+ "px";
+    contFont2[l].style.fontSize=inpFont2[l].value + "px";
+    contFont3[p].style.fontSize=inpFont3[p].value + "px";
+    */
+}
+
+function editar2(n){
+    editables[n].contentEditable="true";
+    editables[n].disignMode='on';
+    botonesXeY[n].style.display="inline-block";
+    fontYColor[n].style.display='inline-block';
+    editables[n].style.outline="#5C9F9E 1px solid"
+    info1=contFont1[n].innerHTML;
+
+    
+}
+function check2(n){
+    editables[n].contentEditable='false';
+    editables[n].disignMode='off';
+    botonesXeY[n].style.display='none';
     editables[n].style.outline="none"
+    fontYColor[n].style.display='none';
+   /* info1=contFont1[n].innerHTML;
+    info2=contFont2[l].innerHTML;
+    info3=contFont3[p].innerHTML;*/
+
+    colorInp1=inpColor1[n].value;
+    contColor1[n].style.color=colorInp1;
+    contFont1[n].style.fontSize=inpFont1[n].value + "px";
+    
+   
+    
+
 }
-function recOrig(n){
-    editables[n].innerHTML=infoOriginal;
-    colorYFont.style.color=colorOrg;
-    editables[n].style.fontSize=fontOrg;
-}
-*/
 
 
+function xEdit2(n){
+  
+   
+    editables[n].contentEditable='false';
+    editables[n].disignMode='off';
+   
+    botonesXeY[n].style.display='none';
+    fontYColor[n].style.display='none';
+    editables[n].style.outline="none"; 
+    contFont1[n].innerHTML=info1;
+    
+    
+    
+   /* contColor1[n].style.color=inpColor1[n].value;
+    contColor2[l].style.color=inpColor2[l].value ;
+    contColor3[p].style.color=colorInp3;
+
+    contFont1[n].style.fontSize=inpFont1[n].value;+ "px";
+    contFont2[l].style.fontSize=inpFont2[l].value + "px";
+    contFont3[p].style.fontSize=inpFont3[p].value + "px";
+    */
+}
+
+
+function editar3(n,l){
+    editables[n].contentEditable="true";
+    editables[n].disignMode='on';
+    botonesXeY[n].style.display="inline-block";
+    fontYColor[n].style.display='inline-block';
+    editables[n].style.outline="#5C9F9E 1px solid"
+    info1=contFont1[n].innerHTML;
+    info2=contFont2[l].innerHTML;
+    
+    
+}
+function check3(n,l){
+    editables[n].contentEditable='false';
+    editables[n].disignMode='off';
+    botonesXeY[n].style.display='none';
+    editables[n].style.outline="none"
+    fontYColor[n].style.display='none';
+   /* info1=contFont1[n].innerHTML;
+    info2=contFont2[l].innerHTML;
+    info3=contFont3[p].innerHTML;*/
+
+    colorInp1=inpColor1[n].value;
+    contColor1[n].style.color=colorInp1;
+    contFont1[n].style.fontSize=inpFont1[n].value + "px";
+    
+    contColor2[l].style.color=inpColor2[l].value ;
+    contFont2[l].style.fontSize=inpFont2[l].value + "px";
+  
+    
+
+}
+
+
+function xEdit3(n,l){
+  
+   
+    editables[n].contentEditable='false';
+    editables[n].disignMode='off';
+   
+    botonesXeY[n].style.display='none';
+    fontYColor[n].style.display='none';
+    editables[n].style.outline="none"; 
+    contFont1[n].innerHTML=info1;
+    contFont2[l].innerHTML=info2;
+   
+    
+    
+   /* contColor1[n].style.color=inpColor1[n].value;
+    contColor2[l].style.color=inpColor2[l].value ;
+    contColor3[p].style.color=colorInp3;
+
+    contFont1[n].style.fontSize=inpFont1[n].value;+ "px";
+    contFont2[l].style.fontSize=inpFont2[l].value + "px";
+    contFont3[p].style.fontSize=inpFont3[p].value + "px";
+    */
+}
