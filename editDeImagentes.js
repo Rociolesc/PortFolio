e7663fafef4db49769b0ff9e2d4818a2f6251b56
Rociolesc <rociolesc99@gmail.com>
2022-03-21 21:59:33 -0300

    solo faltan arreglos finales

---
+++ editDeImagentes.js
@@ -0,0 +1,107 @@
+const editFotoPerf=document.querySelectorAll('#editFotoPerf,#editFotoFUtn,#editFotoFNac,#editFotoFArP');
+const botonXeY=document.querySelectorAll('#editFotoPerf2,#editFotoFUtn2,#editFotoFNac2,#editFotoFArP2')
+const contFile=document.querySelectorAll('#contFile,#contFileFUtn,#contFileFNac,#contFileFArP')
+let inpFile=document.querySelectorAll('#inpFile,#inpFileFUtn,#inpFileFNac,#inpFileFArP')
+/*
+function editFoto(n){
+
+    editFotoPerf[n].style.display="none";
+    botonXeY[n].style.display="inline-block";
+    contFile[n].style.width="45vw";
+    contFile[n].style.paddingLeft="10vw";
+    inpFile[n].style.width="272px";
+}
+function checkFoto(n){
+    botonXeY[n].style.display="none";
+    editFotoPerf[n].style.display="inline-block";
+    contFile[n].style.width="0vw";
+    contFile[n].style.paddingLeft="0px";
+    inpFile[n].style.width="0px";
+
+}
+
+function xEditFoto(n){
+    botonXeY[n].style.display="none";
+    editFotoPerf[n].style.display="inline-block";
+    contFile[n].style.width="0vw";
+    contFile[n].style.paddingLeft="0px";
+    inpFile[n].style.width="0px";
+}*/
+function editFoto(n){
+
+    if (window.innerWidth< 837){
+        console.log("funciona")
+        editFotoPerf[n].style.display="none";
+        botonXeY[n].style.display="inline-block";
+        contFile[n].style.height="284px";
+        contFile[n].style.width="50vw";
+        contFile[n].style.paddingLeft="9vw";
+        contFile[n].style.paddingTop="260px";
+        inpFile[n].style.height="40px";
+        inpFile[n].style.width="272px";
+        inpFile[n].style.transition="all 1.5s";
+    }
+    if (window.innerWidth> 837){
+        console.log("funciona vercion grande")
+    editFotoPerf[n].style.display="none";
+    botonXeY[n].style.display="inline-block";
+    contFile[n].style.width="45vw";
+    contFile[n].style.height="160px";
+    contFile[n].style.paddingLeft="10vw";
+    contFile[n].style.paddingTop="135px";
+    inpFile[n].style.width="272px";
+  
+
+    }
+    
+    
+}
+function checkFoto(n){
+    if (window.innerWidth< 837){
+        editFotoPerf[n].style.display="inline-block";
+        botonXeY[n].style.display="none";
+        contFile[n].style.height="0px";
+        contFile[n].style.width="0vw";
+        contFile[n].style.paddingLeft="0vw";
+        contFile[n].style.paddingTop="0px";
+        inpFile[n].style.height="0px";
+        inpFile[n].style.width="0px";
+        inpFile[n].style.transition="all 0.5s";
+    }
+    if (window.innerWidth> 837){
+    botonXeY[n].style.display="none";
+    editFotoPerf[n].style.display="inline-block";
+    contFile[n].style.width="0vw";
+    contFile[n].style.height="160px";
+    contFile[n].style.paddingLeft="0px";
+    inpFile[n].style.width="0px";
+    inpFile[n].style.height="40px";
+    inpFile[n].style.transition="all 0.5s";
+
+    }
+}
+
+function xEditFoto(n){
+    if (window.innerWidth< 837){
+        editFotoPerf[n].style.display="inline-block";
+        botonXeY[n].style.display="none";
+        contFile[n].style.height="0px";
+        contFile[n].style.width="0vw";
+        contFile[n].style.paddingLeft="0vw";
+        contFile[n].style.paddingTop="0px";
+        inpFile[n].style.height="0px";
+        inpFile[n].style.width="0px";
+        inpFile[n].style.transition="all 0.5s";
+    }
+    if (window.innerWidth> 837){
+    botonXeY[n].style.display="none";
+    editFotoPerf[n].style.display="inline-block";
+    contFile[n].style.width="0vw";
+    contFile[n].style.height="160px";
+    contFile[n].style.paddingLeft="0px";
+    inpFile[n].style.width="0px";
+    inpFile[n].style.height="40px";
+    inpFile[n].style.transition="all 0.5s";
+
+    }
+}
